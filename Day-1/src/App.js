b51d99280b8a46457ba986f56b9b6b337434847f
sameer <111101011+sameer-dixit@users.noreply.github.com>
2024-02-04 01:19:19 +0530

    Initial commit

---
+++ Day-1/src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+
+
+function App() {
+  return (
+    <>
+      
+      <Navbar />
+      <div className="container">
+      <TextForm heading="Enter The Text To Analyze Below"/>
+      </div>
+      
+    </>
+  );
+}
+
+export default App;
